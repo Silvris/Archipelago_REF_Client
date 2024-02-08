97a3cee9f61fb1fd45ffb45a7f3c9a9b7ed8c3bf
Silvris <58583688+Silvris@users.noreply.github.com>
2024-02-08 01:45:16 -0600

    fix major duplication issue, confirm access cross-script

--- reframework/autorun/AP_REF/core.lua
+++ reframework/autorun/AP_REF/core.lua
@@ -1,12 +1,14 @@
+-- TODO: since we had to shove this into its own folder, maybe split this into multiple files?
+local AP = package.loadlib("lua-APClientpp.dll", "luaopen_apclientpp")()
 
-local AP = package.loadlib("lua-apclientpp.dll", "luaopen_apclientpp")()
-
+local AP_REF = {}
+AP_REF.AP = AP
 -- You should set these values from within your mod
-APGameName = ""
-APItemsHandling = 7 -- shouldn't need to change this
-APTags = {"Lua-APClientPP"}
+AP_REF.APGameName = ""
+AP_REF.APItemsHandling = 7 -- shouldn't need to change this
+AP_REF.APTags = {} -- these are reserved for any additional tags, Lua-APClientPP is always applied, and TextOnly when relevant
 
-APColors = {
+AP_REF.APColors = {
     red="EE0000",
     blue="6495ED",
     green="00FF7F",
@@ -30,18 +32,18 @@ local function setDefault (t, d)
 	setmetatable(t, mt)
 end
 
-setDefault(APColors, "FFFFFF")
+setDefault(AP_REF.APColors, "FFFFFF")
 
-APCurrentPlayerColor = "EE00EE"
-APOtherPlayerColor = "FAFAD2"
-APProgessionColor = "AF99EF"
-APUsefulColor = "6D8BE8"
-APFillerColor = "00EEEE"
-APTrapColor = "FA8072"
-APLocationColor = "00FF7F"
-APEntranceColor = "6495ED"
+AP_REF.APCurrentPlayerColor = "EE00EE"
+AP_REF.APOtherPlayerColor = "FAFAD2"
+AP_REF.APProgessionColor = "AF99EF"
+AP_REF.APUsefulColor = "6D8BE8"
+AP_REF.APFillerColor = "00EEEE"
+AP_REF.APTrapColor = "FA8072"
+AP_REF.APLocationColor = "00FF7F"
+AP_REF.APEntranceColor = "6495ED"
 
-function HexToImguiColor(color)
+function AP_REF.HexToImguiColor(color)
 	local r = string.sub(color, 1, 2)
 	local g = string.sub(color, 3, 4)
 	local b = string.sub(color, 5, 6)
@@ -51,7 +53,7 @@ end
 -----------------------------------
 
 -- Utilize at your own peril
-APClient = nil
+AP_REF.APClient = nil
 -----------------------------------
 
 local host = "localhost:38281"
@@ -63,7 +65,7 @@ local textLog = {}
 local connected = false
 local current_text = ""
 
-local DEBUG = false
+local DEBUG = true
 
 local function debug_print(str)
 	if DEBUG then
@@ -93,21 +95,21 @@ local function callback_passthrough_three_arg(pass1, pass2, pass3)
 	a = 1 + 1
 end
 
-on_socket_connected = callback_passthrough
-on_socket_error = callback_passthrough_one_arg
-on_socket_disconnected = callback_passthrough
-on_room_info = callback_passthrough
-on_slot_connected = callback_passthrough_one_arg
-on_slot_refused = callback_passthrough_one_arg
-on_items_received = callback_passthrough_one_arg
-on_location_info = callback_passthrough_one_arg
-on_location_checked = callback_passthrough_one_arg
-on_data_package_changed = callback_passthrough_one_arg
-on_print = callback_passthrough_one_arg
-on_print_json = callback_passthrough_two_arg
-on_bounced = callback_passthrough_one_arg
-on_retrieved = callback_passthrough_three_arg
-on_set_reply = callback_passthrough_one_arg
+AP_REF.on_socket_connected = callback_passthrough
+AP_REF.on_socket_error = callback_passthrough_one_arg
+AP_REF.on_socket_disconnected = callback_passthrough
+AP_REF.on_room_info = callback_passthrough
+AP_REF.on_slot_connected = callback_passthrough_one_arg
+AP_REF.on_slot_refused = callback_passthrough_one_arg
+AP_REF.on_items_received = callback_passthrough_one_arg
+AP_REF.on_location_info = callback_passthrough_one_arg
+AP_REF.on_location_checked = callback_passthrough_one_arg
+AP_REF.on_data_package_changed = callback_passthrough_one_arg
+AP_REF.on_print = callback_passthrough_one_arg
+AP_REF.on_print_json = callback_passthrough_two_arg
+AP_REF.on_bounced = callback_passthrough_one_arg
+AP_REF.on_retrieved = callback_passthrough_three_arg
+AP_REF.on_set_reply = callback_passthrough_one_arg
 
 
 local function set_socket_connected_handler(callback)
@@ -115,71 +117,78 @@ local function set_socket_connected_handler(callback)
 		debug_print("Socket connected")
 		callback()
 	end
-	APClient:set_socket_connected_handler(socket_connected)
+	AP_REF.APClient:set_socket_connected_handler(socket_connected)
 end
 local function set_socket_error_handler(callback)
 	function socket_error_handler(msg)
 		debug_print("Socket error")
 		callback(msg)
 	end
-	APClient:set_socket_error_handler(socket_error_handler)
+	AP_REF.APClient:set_socket_error_handler(socket_error_handler)
 end
 local function set_socket_disconnected_handler(callback)
 	function socket_disconnected_handler()
 		debug_print("Socket disconnected")
 		callback()
 	end
-	APClient:set_socket_disconnected_handler(socket_disconnected_handler)
+	AP_REF.APClient:set_socket_disconnected_handler(socket_disconnected_handler)
 end
 local function set_room_info_handler(callback)
 	function room_info_handler()
 		debug_print("Room info")
 		callback()
-		APClient:ConnectSlot(slot, password, APItemsHandling, APTags, {0, 3, 9})
+		local tags = {"Lua-APClientPP"}
+		if AP_REF.APGameName == "" then
+			table.insert(tags, "TextOnly")
+		end
+		for i, val in ipairs(AP_REF.APTags) do
+			table.insert(tags, val)
+		end
+		AP_REF.APClient:ConnectSlot(slot, password, AP_REF.APItemsHandling, tags, {0, 3, 9})
 	end
-	APClient:set_room_info_handler(room_info_handler)
+	AP_REF.APClient:set_room_info_handler(room_info_handler)
 end
 local function set_slot_connected_handler(callback)
 	function slot_connected_handler(slot_data)
 		debug_print("Slot connected")
 		callback(slot_data)
 	end
-	APClient:set_slot_connected_handler(slot_connected_handler)
+	AP_REF.APClient:set_slot_connected_handler(slot_connected_handler)
 end
 local function set_slot_refused_handler(callback)
 	function slot_refused_handler(reasons)
-		debug_print("Slot refused")
+		debug_print("Slot refused: " .. table.concat(reasons, ", "))
 		callback(reasons)
 	end
-	APClient:set_slot_refused_handler(slot_refused_handler)
+	AP_REF.APClient:set_slot_refused_handler(slot_refused_handler)
 end
 local function set_items_received_handler(callback)
 	function items_received_handler(items)
 		debug_print("Items received")
 		callback(items)
 	end
-	APClient:set_items_received_handler(items_received_handler)
+	AP_REF.APClient:set_items_received_handler(items_received_handler)
 end
 local function set_location_info_handler(callback)
 	function location_info_handler(items)
 		debug_print("Locations info")
 		callback(items)
 	end
-	APClient:set_location_info_handler(location_info_handler)
+	AP_REF.APClient:set_location_info_handler(location_info_handler)
 end
 local function set_location_checked_handler(callback)
 	function location_checked_handler(locations)
 		debug_print("Locations checked")
 		callback(locations)
 	end
-	APClient:set_location_checked_handler(location_checked_handler)
+	AP_REF.APClient:set_location_checked_handler(location_checked_handler)
 end
 local function set_data_package_changed_handler(callback)
 	function data_package_changed_handler(data_package)
 		debug_print("Data package changed")
 		callback(data_package)
 	end
-	APClient:set_data_package_changed_handler(data_package_changed_handler)
+	AP_REF.APClient:set_data_package_changed_handler(data_package_changed_handler)
 end
 local function set_print_handler(callback)
 	function print_handler(msg)
@@ -188,61 +197,58 @@ local function set_print_handler(callback)
 		table.insert(textLog, {{text = msg}})
 		--debug_print(msg)
 	end
-	APClient:set_print_handler(print_handler)
+	AP_REF.APClient:set_print_handler(print_handler)
 end
 local function set_print_json_handler(callback)
 	function print_json_handler(msg, extra)
 		debug_print("Print json")
 		callback(msg, extra)
-		--table.insert(textLog, APClient:render_json(msg, AP.RenderFormat.TEXT))
+		--table.insert(textLog, AP_REF.APClient:render_json(msg, AP.RenderFormat.TEXT))
 		table.insert(textLog, msg)
 	end
-	APClient:set_print_json_handler(print_json_handler)
+	AP_REF.APClient:set_print_json_handler(print_json_handler)
 end
 local function set_bounced_handler(callback)
 	function bounced_handler(bounce)
 		debug_print("Bounce")
 		callback(bounce)
 	end
-	APClient:set_bounced_handler(bounced_handler)
+	AP_REF.APClient:set_bounced_handler(bounced_handler)
 end
 local function set_retrieved_handler(callback)
 	function retrieved_handler(map, keys, extra)
 		debug_print("Retrieved")
 		callback(map, keys, extra)
 	end
-	APClient:set_retrieved_handler(retrieved_handler)
+	AP_REF.APClient:set_retrieved_handler(retrieved_handler)
 end
 local function set_set_reply_handler(callback)
 	function set_reply_handler(message)
 		debug_print("Set Reply")
 		callback(message)
 	end
-	APClient:set_set_reply_handler(set_reply_handler)
+	AP_REF.APClient:set_set_reply_handler(set_reply_handler)
 end
 
 function APConnect(host)
     local uuid = ""
-	if APGameName == "" then
-		table.insert(APTags, "TextOnly")
-	end
-    APClient = AP(uuid, APGameName, host)
+    AP_REF.APClient = AP(uuid, AP_REF.APGameName, host)
 	debug_print("Connecting")
-    set_socket_connected_handler(on_socket_connected)
-    set_socket_error_handler(on_socket_error)
-    set_socket_disconnected_handler(on_socket_disconnected)
-    set_room_info_handler(on_room_info)
-    set_slot_connected_handler(on_slot_connected)
-    set_slot_refused_handler(on_slot_refused)
-    set_items_received_handler(on_items_received)
-    set_location_info_handler(on_location_info)
-    set_location_checked_handler(on_location_checked)
-    set_data_package_changed_handler(on_data_package_changed)
-    set_print_handler(on_print)
-    set_print_json_handler(on_print_json)
-    set_bounced_handler(on_bounced)
-    set_retrieved_handler(on_retrieved)
-    set_set_reply_handler(on_set_reply)
+    set_socket_connected_handler(AP_REF.on_socket_connected)
+    set_socket_error_handler(AP_REF.on_socket_error)
+    set_socket_disconnected_handler(AP_REF.on_socket_disconnected)
+    set_room_info_handler(AP_REF.on_room_info)
+    set_slot_connected_handler(AP_REF.on_slot_connected)
+    set_slot_refused_handler(AP_REF.on_slot_refused)
+    set_items_received_handler(AP_REF.on_items_received)
+    set_location_info_handler(AP_REF.on_location_info)
+    set_location_checked_handler(AP_REF.on_location_checked)
+    set_data_package_changed_handler(AP_REF.on_data_package_changed)
+    set_print_handler(AP_REF.on_print)
+    set_print_json_handler(AP_REF.on_print_json)
+    set_bounced_handler(AP_REF.on_bounced)
+    set_retrieved_handler(AP_REF.on_retrieved)
+    set_set_reply_handler(AP_REF.on_set_reply)
 end
 
 local function DisplayClientCommand(command)
@@ -276,7 +282,7 @@ local function main_menu()
 		imgui.same_line()
 		if connected then
 			if imgui.button("Disconnect") then
-				APClient = nil
+				AP_REF.APClient = nil
 			end
 		else
 			if imgui.button("Connect") then
@@ -293,49 +299,49 @@ local function main_menu()
 					local text_type = val["type"]
 					local color = "FFFFFF"
 					if text_type == "color" then
-						imgui.text_colored(val["text"], HexToImguiColor(APColors[val["color"]]))
+						imgui.text_colored(val["text"], AP_REF.HexToImguiColor(AP_REF.APColors[val["color"]]))
 					elseif text_type == "player_id" then
-						if tonumber(val["text"]) == APClient:get_player_number() then
-							color = APCurrentPlayerColor
+						if tonumber(val["text"]) == AP_REF.APClient:get_player_number() then
+							color = AP_REF.APCurrentPlayerColor
 						else
-							color = APOtherPlayerColor
+							color = AP_REF.APOtherPlayerColor
 						end
-						imgui.text_colored(APClient:get_player_alias(tonumber(val["text"])), HexToImguiColor(color))
+						imgui.text_colored(AP_REF.APClient:get_player_alias(tonumber(val["text"])), AP_REF.HexToImguiColor(color))
 					elseif text_type == "player_name" then
 						-- according to network docs, this only appears when individual is not slot-resolvable?
-						imgui.text_colored(val["text"], HexToImguiColor(APOtherPlayerColor))
+						imgui.text_colored(val["text"], AP_REF.HexToImguiColor(AP_REF.APOtherPlayerColor))
 					elseif text_type == "item_id" then
 						-- resolve item flags
 						if (val["flags"] & 1) > 0 then
-							color = APProgessionColor
+							color = AP_REF.APProgessionColor
 						elseif (val["flags"] & 2) > 0 then
-							color = APUsefulColor
+							color = AP_REF.APUsefulColor
 						elseif (val["flags"] & 4) > 0 then
-							color = APTrapColor
+							color = AP_REF.APTrapColor
 						else
-							color = APFillerColor
+							color = AP_REF.APFillerColor
 						end
-						-- TODO: become 1933 compliant once a new version of lua-apclientpp releases
-						imgui.text_colored(APClient:get_item_name(tonumber(val["text"])), HexToImguiColor(color))
+						-- TODO: become 1933 compliant once a new version of lua-AP_REF.APClientpp releases
+						imgui.text_colored(AP_REF.APClient:get_item_name(tonumber(val["text"])), AP_REF.HexToImguiColor(color))
 					elseif text_type == "item_name" then
 						-- resolve item flags
 						if val["flags"] & 1 then
-							color = APProgessionColor
+							color = AP_REF.APProgessionColor
 						elseif val["flags"] & 2 then
-							color = APUsefulColor
+							color = AP_REF.APUsefulColor
 						elseif val["flags"] & 4 then
-							color = APTrapColor
+							color = AP_REF.APTrapColor
 						else
-							color = APFillerColor
+							color = AP_REF.APFillerColor
 						end
-						imgui.text_colored(val["text"], HexToImguiColor(color))
+						imgui.text_colored(val["text"], AP_REF.HexToImguiColor(color))
 					elseif text_type == "location_id" then
-						-- TODO: become 1933 compliant once a new version of lua-apclientpp releases
-						imgui.text_colored(APClient:get_location_name(tonumber(val["text"])), HexToImguiColor(APLocationColor))
+						-- TODO: become 1933 compliant once a new version of lua-AP_REF.APClientpp releases
+						imgui.text_colored(AP_REF.APClient:get_location_name(tonumber(val["text"])), AP_REF.HexToImguiColor(AP_REF.APLocationColor))
 					elseif text_type == "location_name" then
-						imgui.text_colored(val["text"], HexToImguiColor(APLocationColor))
+						imgui.text_colored(val["text"], AP_REF.HexToImguiColor(AP_REF.APLocationColor))
 					elseif text_type == "entrance_name" then
-						imgui.text_colored(val["text"], HexToImguiColor(APEntranceColor))
+						imgui.text_colored(val["text"], AP_REF.HexToImguiColor(AP_REF.APEntranceColor))
 					else
 						imgui.text(val["text"])
 					end
@@ -362,7 +368,7 @@ local function main_menu()
 					DisplayClientCommand(string.sub(current_text, 2))
 					current_text = ""
 				elseif ap ~= nil then
-					APClient:Say(current_text)
+					AP_REF.APClient:Say(current_text)
 					current_text = ""
 				end
 			end
@@ -374,14 +380,14 @@ end
 
 local function SaveConfig()
     config = {}
-    config["APCurrentPlayerColor"] = APCurrentPlayerColor
-    config["APOtherPlayerColor"] = APOtherPlayerColor
-    config["APProgessionColor"] = APProgessionColor
-    config["APUsefulColor"] = APUsefulColor
-    config["APFillerColor"] = APFillerColor
-    config["APTrapColor"] = APTrapColor
-    config["APLocationColor"] = APLocationColor
-	config["APEntranceColor"] = APEntranceColor
+    config["APCurrentPlayerColor"] = AP_REF.APCurrentPlayerColor
+    config["APOtherPlayerColor"] = AP_REF.APOtherPlayerColor
+    config["APProgessionColor"] = AP_REF.APProgessionColor
+    config["APUsefulColor"] = AP_REF.APUsefulColor
+    config["APFillerColor"] = AP_REF.APFillerColor
+    config["APTrapColor"] = AP_REF.APTrapColor
+    config["APLocationColor"] = AP_REF.APLocationColor
+	config["APEntranceColor"] = AP_REF.APEntranceColor
     if not json.dump_file("AP_REF.json", config, 4) then
         print("Config cannot be saved!")
     end
@@ -391,28 +397,28 @@ local function ReadConfig()
     config = json.load_file("AP_REF.json")
     if config ~= nil then
         if config["APCurrentPlayerColor"] ~= nil then
-            APCurrentPlayerColor = config["APCurrentPlayerColor"]
+            AP_REF.APCurrentPlayerColor = config["APCurrentPlayerColor"]
         end
         if config["APOtherPlayerColor"] ~= nil then
-            APOtherPlayerColor = config["APOtherPlayerColor"]
+            AP_REF.APOtherPlayerColor = config["APOtherPlayerColor"]
         end
         if config["APProgessionColor"] ~= nil then
-            APProgessionColor = config["APProgessionColor"]
+            AP_REF.APProgessionColor = config["APProgessionColor"]
         end
         if config["APUsefulColor"] ~= nil then
-            APUsefulColor = config["APUsefulColor"]
+            AP_REF.APUsefulColor = config["APUsefulColor"]
         end
         if config["APFillerColor"] ~= nil then
-            APFillerColor = config["APFillerColor"]
+            AP_REF.APFillerColor = config["APFillerColor"]
         end
         if config["APTrapColor"] ~= nil then
-            APTrapColor = config["APTrapColor"]
+            AP_REF.APTrapColor = config["APTrapColor"]
         end
         if config["APLocationColor"] ~= nil then
-            APLocationColor = config["APLocationColor"]
+            AP_REF.APLocationColor = config["APLocationColor"]
         end
 		if config["APEntranceColor"] ~= nil then
-			APEntranceColor = config["APEntranceColor"]
+			AP_REF.APEntranceColor = config["APEntranceColor"]
 		end
     else
         SaveConfig()
@@ -437,13 +443,13 @@ ReadConfig()
 
 re.on_pre_application_entry("UpdateBehavior", function() 
     --main loop access
-	if ap ~= nil then
-		if APClient:get_state() == AP.State.DISCONNECTED then
+	if AP_REF.APClient ~= nil then
+		if AP_REF.APClient:get_state() == AP.State.DISCONNECTED then
 			connected = false
 		else
 			connected = true
 		end
-		APClient:poll()
+		AP_REF.APClient:poll()
 	else
 		connected = false
 	end
@@ -452,3 +458,5 @@ end)
 re.on_config_save(function()
     SaveConfig()
 end)
+
+return AP_REF
